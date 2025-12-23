ffd883132ba86c912600ac4dda92a168265226c5
nuggets <Yaohaimeng@proton.me>
2025-12-24 02:12:09 +0800

    first commit

--- src/main/java/org/yhm/spark2/service/Impl/SparkServiceImpl.java
+++ src/main/java/org/yhm/spark2/service/Impl/SparkServiceImpl.java
@@ -25,8 +25,14 @@ public class SparkServiceImpl implements SparkService {
         // val df = spark.read.option("header", "true").option("inferSchema", "true").csv("hdfs://lavm-yl28gyaaew:9001/data/ais/raw/2025/06/")
         Dataset<Row> df = spark.read().option("header", "true").option("inferSchema", "true").csv("hdfs://lavm-yl28gyaaew:9001/data/ais/raw/2025/06/");
         // 2. 过滤数据 筛选出 BaseDateTime 在 2025-06-26 00:00:00 到 2025-06-26 00:00:10 的所有记录，然后打印这些记录
-        Dataset<Row> filteredDf = df.filter(df.col("BaseDateTime").between(start, end));
+        Dataset<Row> filteredDf = df.filter(df.col("base_date_time").between(start, end));
         filteredDf.show();
-        return filteredDf;
+
+        // 将DataFrame转换为JSON字符串数组，便于Web API返回
+        String[] jsonRows = filteredDf.toJSON().collectAsList().toArray(new String[0]);
+        log.info("queryData jsonRows: " + jsonRows);
+
+        spark.close(); // 关闭SparkSession以释放资源
+        return jsonRows;
     }
 }
